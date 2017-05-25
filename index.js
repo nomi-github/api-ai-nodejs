4ee0aa73799d4c0a9b7247e95a107af74eeb8b91
nomi-github <nomindalain@gmail.com>
2017-05-25 17:08:31 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,51 @@
-var express = require('express');
-var app = express();
+var express = require("@runkit/runkit/express-endpoint/1.0.0");
+var consoleLogWrapper = require("console-log-wrapper")
+var app = express(exports);
+var URL = require('url');
+var API_AI = require('apiai');
+var appapiai = API_AI(process.env.API_AI_KEY);
 
-app.set('port', (process.env.PORT || 5000));
+var data = {
+  "messages": [
+    {
+      "attachment": {
+        "type": "image",
+        "payload": {
+          "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Face-grin.svg/200px-Face-grin.svg.png"
+        }
+      }
+    },
+    {
+        "text":"Response from api.ai"
+    }
+  ]
+}
 
-app.use(express.static(__dirname + '/public'));
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
+app.get("/", (req, res) => {
+   //res.send(data);
+   
+     var request = app.textRequest('test', {
+        sessionId: 'fjeofjoajfoiejfojewofioa'
+    });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+    request.on('response', function(response) {
+       var message = { messages: [{ "text": "result" || result.fulfillment.speech }] };
+      //  var responseText = response.result.fulfillment.speech;
+        var responseMessages = response.result.fulfillment.messages[0].payload;
+        res.send(message);
+      //  res.send(responseMessages);
+    });
+
+    request.on('error', function(error) {
+        var message = { messages: [{ "text": "error" || error }] };
+         console.error(sender, 'Error while call to api.ai', error);
+       // var message = JSON.stringify({ error: error });
+      //  res.send(message);
+    });
+    res.send(data);
+   // request.end();
+})
 
 
